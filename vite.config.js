dbd535eaa85e82779652c7ff0aa665fcd251e2ef
H_硕 <2393097587@qq.com>
2025-07-22 11:21:54 +0800

    fix: 修复cicd

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/my-github-actions/', 
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.jsx?$/,
